e3e06e5550cf958df4874713935459cc4b078c71
Lotte <0942533@hr.nl>
2021-06-13 17:43:02 +0200

    Add home markup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
+import { Helmet } from 'react-helmet';
 import { Row, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import IntroImage from '../images/intro.png';
 
 const Home = () => (
-        <Row className="home">
-            <div className="home-introduction col-lg-6 col-sm-12 col-xs-12">
-                <h1 class="home__title">Toegankelijker in een paar stappen</h1>
+    <Row className="home">
+        <Helmet>
+            <title>Accessibility Training</title>
+        </Helmet>
 
-                <p class="home__text">100% gratis training over het digitaal toegankelijk maken van websites/apps. Speciaal ontwikkeld voor ondernemers binnen het MKB.</p>
+        <div className="home-introduction hidden-hover col-lg-6 col-sm-12 col-xs-12">
+            <h1 class="home__title">Toegankelijker in een paar stappen</h1>
 
-                <Button className="home__button btn-orange">Start de training</Button>
-            </div>
+            <p class="home__text">100% gratis training over het digitaal toegankelijk maken van websites/apps. Speciaal ontwikkeld voor ondernemers binnen het MKB.</p>
 
-            <div className="home__image col-lg-6 col-sm-12 col-xs-12">
-                <img src={IntroImage} class="home__image-size" />
-            </div>
-        </Row>
-   
-         /* <h1>Hello from Home</h1>
-         <button><Link to="/introduction">Start training</Link></button> */
+            <Link to="/introduction"><Button className="home__button btn-orange">Start training</Button></Link>
+        </div>
+
+        <div className="home__image col-lg-6 col-sm-12 col-xs-12">
+            <img src={IntroImage} alt="" class="home__image-size" />
+        </div>
+    </Row>
 );
 
 export default Home;
