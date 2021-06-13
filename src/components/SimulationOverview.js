bbdab683ef235096b44e86eae3eeb25e47842689
Lotte <0942533@hr.nl>
2021-06-13 23:02:46 +0200

    Add markup component simulation overview

---
+++ src/components/SimulationOverview.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import { Row, Button, Container } from 'react-bootstrap';
+
+export default function QuizIntro() {
+
+    return (
+        <div>
+            <div class="simulation-wrapper">
+                <Helmet>
+                    <title>Simulaties | Accessibility Training</title>
+                </Helmet>
+
+                <Row>
+                    <div className="simulation__introduction col-lg-6 col-sm-12 col-xs-12">
+                        <h1 class="u-text-title">Soorten visuele beperkingen</h1>
+
+                        <ul class="vision-description">
+                            <li><strong>Slechtziend</strong> - Gezichtsscherpte minder dan 30%</li>
+                            <span>- Kan niet worden gecorrigeerd door het dragen van een bril of contactlenzen</span>
+                            <li><strong>Zeer slechtziend</strong> - Gezichsscherpte minder dan 10%</li>
+                            <li><strong>Maatschappelijk blind</strong> - Gezichsscherpte minder dan 5%</li>
+                            <span>- Je kunt nog licht en donker onderscheiden en de omtrek van mensen en objecten zien</span>
+                            <li><strong>Blind</strong> - Gezichsscherpte minder dan 2%</li>
+                            <span>- Je kunt niets zien, ook geen licht</span>
+                        </ul>
+
+                        <div class="assignment">
+                            <h2 class="u-text-title">Opdracht</h2>
+                            <p class="u-text-assignment">Benieuwd naar hoe een persoon met visuele beperking een webshop waarneemt? Kies één van de verschillende soorten visuele beperkingen uit en ervaar het zelf!</p>
+                            <Link to="/quizOverview"><Button>Sla opdracht over</Button></Link>
+                        </div>
+                    </div>
+
+                    <div className="simulation__answers col-lg-6 col-sm-12 col-xs-12">
+                    <h2 class="u-text-title">Opties visuele beperkingen</h2>
+                    <Button href="https://accessibility-game.netlify.app/levels/6/intro/" target="blank" className="btn-choice">Slechtziend</Button><br/>
+                    <Button href="https://accessibility-game.netlify.app/levels/3/intro/" target="blank" className="btn-choice">Blind</Button>
+                    
+                    <Link to="/quizOverview"><Button className="btn-simulation">Volgende</Button></Link>
+                    </div>
+                </Row>
+
+            </div>
+        </div>
+    );
+}
