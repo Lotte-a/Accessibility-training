5110f768db0470cb14439086a7cbd9e7083983de
Lotte <0942533@hr.nl>
2021-06-13 23:03:44 +0200

    Add markup component Intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,48 +5,56 @@ import { Button, Container } from 'react-bootstrap';
 import Modal from 'react-modal';
 
 import ReactPlayer from 'react-player/youtube'
-
-
-
 export default function QuizIntro() {
 
     const [closeModal, setCloseModal] = useState(true);
+    const [autoPlay, setAutoPlay] = useState(false);
 
     const handleCloseModal = () => {
         setCloseModal(false);
+        setAutoPlay(true);
     }
 
     return (
         <div>
-            <div class="page-margin">
+            <div class="intro intro-wrapper">
                 <Helmet>
                     <title>Introductie | Accessibility Training</title>
                 </Helmet>
 
                 <Modal
                     isOpen={closeModal}
+                    className="intro-modal"
                     aria={{
                         labelledby: "heading",
                         describedby: "full_description"
-                    }}>
-                    <h1 id="heading">Alert</h1>
+                    }}
+                  >
+                    <h1 class="intro__title">Hierna volgt een introductie video waarin de volgende onderwerpen behandeld zullen worden:</h1>
                     <div id="full_description">
-                        <p>Description goes here.</p>
-                        <Button onClick={handleCloseModal}>Start de video</Button>
+                        <ol>
+                            <li>Persoonlijke ervaring visueel beperkte</li>
+                            <li>Wat is digitale toegankelijkheid?</li>
+                            <li>Verschillende soorten beperkingen</li>
+                            <li>Demonstratie ontoegankelijke website</li>
+                        </ol> 
                     </div>
+
+                    <Button class="intro__button-start" onClick={handleCloseModal}>Start de video</Button>
                 </Modal>
 
                 <div className='player-wrapper'>
                     <ReactPlayer
                         className='react-player'
-                        url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+                        url='https://youtu.be/cBsES4hgNsw'
                         width='100%'
                         height='100%'
+                        playing={autoPlay}
                     />
                 </div>
 
-                <div class="hidden-hover">
-                    <Link to="/quizOverview"><Button className="intro__button btn-orange">Volgende</Button></Link>
+                <div class="intro-btn hidden-hover">
+                    <Link to="/simulationOverview"><Button className="intro__button-next btn-orange">Volgende</Button></Link>
                 </div>
             </div>
         </div>
