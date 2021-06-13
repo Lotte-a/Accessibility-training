b166eebf0040c52c26c9d7535f81ff531ab22e44
Lotte <0942533@hr.nl>
2021-06-13 23:03:16 +0200

    Add markup component ColorContrast

---
+++ src/components/quiz/ColorContrast.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
+
+export default function QuizColorContrast() {
+	return (
+		<h1>Hier komt iets over Color contrast</h1>
+	);
+}
